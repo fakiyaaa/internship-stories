659e28ee62aa48038bfb2a446abb717d6469dcbf
fakiyaaa <149144145+fakiyaaa@users.noreply.github.com>
2026-03-04 21:13:27 -0300

    like localstorage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,51 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+
 import Header from "./components/Header"
 import HomePage from "./components/HomePage"
 import StoryPage from "./components/StoryPage"
 
+
 function App() {
   const [page, setPage] = useState("home")
   const [selectedStory, setSelectedStory] = useState(null)  
-  const [stories, setStories] = useState(
-    [
+  const [stories, setStories] = useState(() => {
+
+    const savedStories = localStorage.getItem("stories")
+    return savedStories 
+    ? JSON.parse(savedStories) 
+    : [
+          {
+        id: 1,
+        company: "Netflix",
+        role: "Software Engineer Intern",
+        headline: "FAANG internship journey",
+        summary: "My experience working at Netflix.",
+        likes: 1,
+        saved: false 
+      },
       {
-    id: 1,
-    company: "Netflix",
-    role: "Software Engineer Intern",
-    headline: "FAANG internship journey",
-    summary: "My experience working at Netflix.",
-    likes: 1,
-    saved: false 
-  },
-  {
-    id: 2,
-    company: "Google",
-    role: "Software Engineering Intern",
-    headline: "Building ML pipelines",
-    summary: "I worked on search ranking systems.",
-    likes: 4,
-    saved: false 
-  },
-  {
-    id: 3,
-    company: "NASA",
-    role: "Aerospace Intern",
-    headline: "Working on spacecraft simulations",
-    summary: "Thermal modeling for space missions.",
-    likes: 10,
-    saved: false 
-  }
-    ])
+        id: 2,
+        company: "Google",
+        role: "Software Engineering Intern",
+        headline: "Building ML pipelines",
+        summary: "I worked on search ranking systems.",
+        likes: 4,
+        saved: false 
+      },
+      {
+        id: 3,
+        company: "NASA",
+        role: "Aerospace Intern",
+        headline: "Working on spacecraft simulations",
+        summary: "Thermal modeling for space missions.",
+        likes: 10,
+        saved: false 
+      }
+      ]
+    })
+    useEffect(() => {
+      localStorage.setItem("stories", JSON.stringify(stories))
+    }, [stories])
 
     function handleLike(id) {
     setStories(
